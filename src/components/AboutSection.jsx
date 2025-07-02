bcc67c7e711610c51d5b8f22f3fc4cd548d2b2da
Sumit Rautela <rautelasumit364@gmail.com>
2025-07-02 14:06:18 +0530

    Updated code with new changes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -39,7 +39,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="./Sumit_Rautela.pdf"
+                href="./Sumit_Rautela_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download Resume
